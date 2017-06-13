6e27ffe51e9d5f10d78c52a7b5e07da3d63ca11f
hec <hecaton.prco@gmail.com>
2017-06-14 00:13:45 +0900

    FIDの簡単な説明と実装

---
+++ FID.h
@@ -0,0 +1,25 @@
+// FID.h
+#ifndef __FID_H_INCLUDED__
+#define __FID_H_INCLUDED__
+
+class FID
+{
+private:
+	const size_t smallSize = 8; // lg(n) / 2
+	const size_t largeSize = 256; // lg(n) ^ 2
+	const size_t popCountLength = 256; // 2^smallSize
+
+	uint16_t* blockLarge; // lg n bit per element
+	uint8_t* blockSmall; // lg largeSize bit per element
+	uint8_t* originalBit;  // total: ceil(n / 8) * 8 bit
+	size_t originalBitLength;
+
+public:
+	~FID();
+	FID(const uint8_t* BIT);
+	uint16_t rank(int pos, int digit);
+	uint16_t select(int idx, int digit);
+	uint8_t popCount(uint8_t bits);
+};
+
+#endif
---
+++ README.md
@@ -0,0 +1,6 @@
+完備辞書
+
+ビット列に対する定数時間のrankとselectを実現できるデータ構造
+
+rank(pos,digit): ビット列の先頭から位置 pos までに digit のビットの個数
+select(idx,digit): ビット列の先頭から見て、idx+1 個目の digit のビットの位置
